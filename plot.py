16e6246678a62795f09e285a5ce8801bd99413cf
Ivan Bui <ivan_bui@usiglobal.com>
2024-04-02 11:31:40 +0700

    fix plot

--- plot.py
+++ plot.py
@@ -1,12 +1,13 @@
-import json
+import yaml
 import matplotlib.pyplot as plt
 import matplotlib
 import seaborn as sns
 matplotlib.use('Agg')
 
 
-CONFIG = json.load(open('config.json'))
+CONFIG = yaml.load(open('config.yml'), Loader=yaml.FullLoader)
 MONTHS = CONFIG['months']
+COLORS = CONFIG['colors']
 
 
 def display_num_of_columns(ax):
@@ -35,10 +36,10 @@ def plot_num_tasks_by_mtd(data, column, title, filename):
     """
     Plot clustered stacked bar chart of number of tasks by month and column
     """
-    temp = data[column].groupby(data['Current Month'], observed=True).value_counts().unstack()
+    temp = data[column].groupby(data['Start Month'], observed=True).value_counts().unstack()
     temp = temp.reindex(MONTHS, axis=0)
     temp.fillna(0, inplace=True)
-    ax = temp.plot(kind='bar', stacked=True, colormap='Set2', figsize=(12, 8))
+    ax = temp.plot(kind='bar', stacked=True, figsize=(12, 8), color=COLORS)
     ax = display_num_of_columns(ax)
     ax.set_yticks(list(map(int, ax.get_yticks())))
     ax.title.set_text(title)
@@ -52,48 +53,50 @@ def plot_pie_done(data, title, filename):
     """
     Plot pie chart of percentage of done tasks
     """
-    temp = data[data['Current Month'] == data['Current Month'].max()]
-    count_done = temp[temp['Bucket Name'] == 'Done'].shape[0]
-    count_not_done = temp[temp['Bucket Name'] != 'Done'].shape[0]
-    try:
-        ax = plt.figure(figsize=(8, 6))
-        plt.pie([count_done, count_not_done],
-                labels=['Done', 'Not done'], autopct='%1.1f%%',
-                startangle=140, colors=['#66b3ff', '#ff9999'])
-        plt.title(title)
-        plt.savefig(filename, bbox_inches='tight')
-    except Exception as e:
-        pass
+    count_done = data[data['Bucket Name'] == 'Done'].shape[0]
+    count_not_done = data[data['Bucket Name'] != 'Done'].shape[0]
+
+    if count_not_done == 0:
+        count_done = 1
+    elif count_done == 0:
+        count_done_not = 1
+    ax = plt.figure(figsize=(8, 6))
+    plt.pie([count_done, count_not_done],
+            labels=['Done', 'Not done'], autopct='%1.1f%%',
+            startangle=140, colors=["#5DE2E7", "#ff9999"])
+    plt.title(title)
+    plt.savefig(filename, bbox_inches='tight')
 
 
 def plot_pie_late(data, title, filename):
     """
     Plot pie chart of percentage of late tasks
     """
-    temp = data[data['Current Month'] == data['Current Month'].max()]
-    count_late = temp[temp['Late'] == True].shape[0]
-    count_not_late = temp.shape[0] - count_late
-    try:
-        ax = plt.figure(figsize=(8, 6))
-        plt.pie([count_late, count_not_late], labels=['Late', 'Not late'], autopct='%1.1f%%',
-                startangle=140, colors=['#ff9999', '#66b3ff'])
-        plt.title(title)
-        plt.savefig(filename, bbox_inches='tight')
-    except Exception as e:
-        pass
+    count_late = data[data['Bucket Name'] == 'Ongoing-Risky'].shape[0]
+    count_not_late = data[~data['Bucket Name'].isin(['Ongoing-Risky', 'Done'])].shape[0]
+
+    if count_late == 0:
+        count_not_late = 1
+    elif count_not_late == 0:
+        count_late = 1
+    ax = plt.figure(figsize=(8, 6))
+    plt.pie([count_late, count_not_late], labels=['Late', 'Not late'], autopct='%1.1f%%',
+            startangle=140, colors=["#ff9999", "#5DE2E7"])
+    plt.title(title)
+    plt.savefig(filename, bbox_inches='tight')
 
 
-def plot_num_tasks_by_department(data, title, filename):
+
+def plot_num_tasks_by_unit(data, title, filename):
     """
-    Plot clustered stacked bar chart of number of tasks by department of current month
+    Plot clustered stacked bar chart of number of tasks by unit
     """
-    temp = data[data['Current Month'] == data['Current Month'].max()]
-    temp = temp['Bucket Name'].groupby(temp['Department'], observed=True).value_counts().unstack()
-    ax = temp.plot(kind='bar', stacked=True, colormap='Set2', figsize=(12, 8))
+    temp = data['Bucket Name'].groupby(data['Unit'], observed=True).value_counts().unstack()
+    ax = temp.plot(kind='bar', stacked=True, figsize=(12, 8), color=COLORS)
     ax = display_num_of_columns(ax)
     plt.yticks(list(map(int, plt.yticks()[0])))
     plt.title(title)
-    plt.xlabel('Department')
+    plt.xlabel('Unit')
     plt.ylabel('Number of tasks')
     plt.xticks(rotation=0)
     plt.yticks(list(map(int, plt.yticks()[0])))
@@ -104,29 +107,11 @@ def plot_count_by_ytd(data, column, title, filename):
     """
     Plot bar chart of count of tasks by column of current year to date
     """
-    temp = data[data['Current Month'] == data['Current Month'].max()]
     ax = plt.figure(figsize=(12, 8))
-    ax = sns.countplot(y=column, data=temp, palette='Set2')
+    ax = sns.countplot(y=column, data=data, palette=COLORS)
     ax = display_num_of_columns_horizontal(ax)
     plt.title(title)
     plt.ylabel(column)
     plt.xlabel('Number of tasks')
     plt.xticks(list(map(int, plt.xticks()[0])))
-    plt.savefig(filename, bbox_inches='tight')
-
-
-def plot_count_priority(data, title, filename):
-    """
-    Plot bar chart of count of tasks by priority of current month
-    """
-    temp = data[data['Current Month'] == data['Current Month'].max()]
-    temp = temp['Late'].groupby(temp['Priority']).value_counts().unstack()
-    temp.fillna(0, inplace=True)
-    ax = temp.plot(kind='bar', stacked=True, figsize=(8, 6), color=['#66b3ff', '#ff9999'])
-    ax = display_num_of_columns(ax)
-    plt.title(title)
-    plt.xlabel('Priority')
-    plt.ylabel('Number of tasks')
-    plt.xticks(rotation=0)
-    plt.yticks(list(map(int, plt.yticks()[0])))
     plt.savefig(filename, bbox_inches='tight')
