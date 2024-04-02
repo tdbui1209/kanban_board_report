062f7a851d4f0e118f86de2213d59120f1604795
Ivan Bui <ivan_bui@usiglobal.com>
2024-04-02 11:32:30 +0700

    add generate.bat

--- generate.bat
+++ generate.bat
@@ -1,11 +1,5 @@
 @ECHO OFF
-echo "  _____                       ___       _"
-echo "  \_   \__   ____ _ _ __     / __\_   _(_)"
-echo "   / /\/\ \ / / _` | '_ \   /__\// | | | |"
-echo "/\/ /_   \ V / (_| | | | | / \/  \ |_| | |"
-echo "\____/    \_/ \__,_|_| |_| \_____/\__,_|_|"
-echo "                                          "
 call .\venv\Scripts\activate
 python .\main.py
-echo Done!
+echo "Done!"
 pause
